886dc71e8106b077e2672da880836a4dfe42cbe6
saloni <salonikansal2244@gmail.com>
2025-05-05 00:28:11 +0530

    done

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,8 +3,6 @@ import axios from "axios";
 const API_URI = "https://gmail-clone-otb6.onrender.com";
 
 const API_GMAIL = async (serviceUrlObject, requestData = {}, type) => {
-  const { params, urlParams, ...body } = requestData;
-
   return await axios({
     method: serviceUrlObject.method,
     url: `${API_URI}/${serviceUrlObject.endpoint}/${type}`,
